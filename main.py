ac54af6573a0aa57ee2f70b6941e4657b6f2a69d
Wendi Zhang <wendiz@google.com>
2017-10-30 17:04:34 -0700

    Fix syntax endswith in main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class StaticHandler(webapp2.RequestHandler):
     elif kwargs['path'].endswith('.css'):
       self.response.headers['Content-Type'] = 'text/css'
     origin = self.request.headers['Origin']
-    if origin.startswith('http://localhost') or origin.endsWith('.googleplex.com'):
+    if origin.startswith('http://localhost') or origin.endswith('.googleplex.com'):
       self.response.headers['Access-Control-Allow-Origin'] = origin
       self.response.headers['Access-Control-Allow-Credentials'] = 'true'
     file = open('dist/' + kwargs['path'], 'rb')
